01d2e654a470525513822d9177b053dd1231fd59
masororasghar1 <u1118456@umail.usq.edu.au>
2020-08-05 18:53:38 +0500

    content added with roughstyling

--- Index.php
+++ Index.php
@@ -97,6 +97,104 @@ function showSlides(n) {
   <span class="dot" onclick="currentSlide(3)"></span>
 </div>
 
+<div id="ahm_content">
+    	<!--
+        <div id="ahm_content_left">
+        	<div class="ahm_content_left_section">
+            	<h1>Categories</h1>
+                <ul>
+                    <li><a href="subpage.html">Donec accumsan urna</a></li>
+                    <li><a href="subpage.html">Proin vulputate justo</a></li>
+                    <li><a href="#">In sed risus ac feli</a></li>
+                    <li><a href="#">Aliquam tristique dolor</a></li>
+                    <li><a href="#">Maece nas metus</a></li>
+                    <li><a href="#">Sed pellentesque placerat</a></li>
+                    <li><a href="#">Suspen disse</a></li>
+                    <li><a href="#">Maece nas metus</a></li>
+                    <li><a href="#">In sed risus ac feli</a></li>
+            	</ul>
+            </div>
+			<div class="ahm_content_left_section">
+            	<h1>Bestsellers</h1>
+                <ul>
+                    <li><a href="#">Vestibulum ullamcorper</a></li>
+                    <li><a href="#">Maece nas metus</a></li>
+                    <li><a href="#">In sed risus ac feli</a></li>
+                    <li><a href="#">Praesent mattis varius</a></li>
+                    <li><a href="#">Maece nas metus</a></li>
+                    <li><a href="#">In sed risus ac feli</a></li>
+                    <li><a href="http://www.flashmo.com" target="_parent">Flash Templates</a></li>
+                    <li><a href="http://www.templatemo.com" target="_parent">CSS Templates</a></li>
+                    <li><a href="http://www.webdesignmo.com" target="_parent">Web Design</a></li>
+                    <li><a href="http://www.photovaco.com" target="_parent">Free Photos</a></li>
+            	</ul>
+            </div>
+            
+             <!-- end of content left -->
+        
+        <div id="ahm_content_right">
+        	<div class="ahm_product_box">
+            	<h1>Photography  <span>(by Best Author)</span></h1>
+   	      <img src="images/templatemo_image_01.jpg" alt="image" />
+                <div class="product_info">
+                	<p>Etiam luctus. Quisque facilisis suscipit elit. Curabitur...</p>
+                  <h3>$55</h3>
+                    <div class="buy_now_button"><a href="subpage.html">Buy Now</a></div>
+                    <div class="detail_button"><a href="subpage.html">Detail</a></div>
+                </div>
+                <div class="cleaner">&nbsp;</div>
+            </div>
+            
+            <div class="cleaner_with_width">&nbsp;</div>
+            
+            <div class="ahm_product_box">
+            	<h1>Cooking  <span>(by New Author)</span></h1>
+       	    <img src="images/ahm_image_02.jpg" alt="image" />
+                <div class="product_info">
+                	<p>Aliquam a dui, ac magna quis est eleifend dictum.</p>
+                    <h3>$35</h3>
+                    <div class="buy_now_button"><a href="subpage.html">Buy Now</a></div>
+                    <div class="detail_button"><a href="subpage.html">Detail</a></div>
+                </div>
+                <div class="cleaner">&nbsp;</div>
+            </div>
+            
+            <div class="cleaner_with_height">&nbsp;</div>
+            
+            <div class="ahm_product_box">
+            	<h1>Gardening <span>(by Famous Author)</span></h1>
+   	      <img src="images/ahm_image_03.jpg" alt="image" />
+                <div class="product_info">
+                	<p>Ut fringilla enim sed turpis. Sed justo dolor, convallis at.</p>
+                    <h3>$65</h3>
+                    <div class="buy_now_button"><a href="subpage.html">Buy Now</a></div>
+                    <div class="detail_button"><a href="subpage.html">Detail</a></div>
+                </div>
+                <div class="cleaner">&nbsp;</div>
+            </div>
+            
+            <div class="cleaner_with_width">&nbsp;</div>
+            
+            <div class="ahm_product_box">
+            	<h1>Sushi Book  <span>(by Japanese Name)</span></h1>
+            	<img src="images/ahm_image_04.jpg" alt="image" />
+                <div class="product_info">
+                	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <h3>$45</h3>
+                    <div class="buy_now_button"><a href="subpage.html">Buy Now</a></div>
+                    <div class="detail_button"><a href="subpage.html">Detail</a></div>
+                </div>
+                <div class="cleaner">&nbsp;</div>
+            </div>
+            
+            <div class="cleaner_with_height">&nbsp;</div>
+            
+            <a href="subpage.html"><img src="images/templatemo_ads.jpg" alt="ads" /></a>
+        </div> <!-- end of content right -->
+    
+    	<div class="cleaner_with_height">&nbsp;</div>
+    </div> <!-- end of content -->	
+	
 <div id="ahm_footer">
     
 	       <a href="subpage.html">Home</a> | <a href="subpage.html">Search</a> | <a href="subpage.html">Books</a> | <a href="#">New Releases</a> | <a href="#">FAQs</a> | <a href="#">Contact Us</a><br />

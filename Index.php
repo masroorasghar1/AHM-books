e69a00db90a0a845f1e71b7b9654d10706c98152
masororasghar1 <u1118456@umail.usq.edu.au>
2020-08-05 15:40:49 +0500

    init

---
+++ Index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+<title>AHM Books</title>
+<style>
+
+body{
+	margin: 0;
+	padding: 0;
+	line-height: 1.5em;
+	font-family: Verdana, Arial, san-serif;
+	font-size: 11px;
+	color: #ffffff;
+	background: #DAF7A6;
+}
+	
+#ahm_container{
+	width: 960px;
+	margin: 0 auto;
+	padding: 0 10px;
+	background: #1c1c1b url(images/templatemo_bg.jpg) repeat-y;
+}	
+/* menu */
+
+#ahm_menu {
+	clear: both;
+	width: 960px;
+	height: 45px;
+	background: #800080;
+}
+
+#ahm_menu ul {
+	padding: 13px 0 0 0;
+	margin: 0;
+	list-style: none;
+}
+
+#ahm_menu ul li{
+	display: inline;
+}
+
+#ahm_menu ul li a{
+	float: left;
+	padding: 0 20px;
+	font-size: 12px;
+	font-weight: bold;
+	text-align: center;
+	text-decoration: none;
+	color: #FFFFFF;
+}
+
+#ahm_menu li a:hover, #ahm_menu li .current{
+	color: #fcf88e;
+}
+	
+	
+</style>	
+	
+</head>
+
+<body>
+<div id="ahm_container">
+	<div id="ahm_menu">
+    	<ul>
+            <li><a href="index.html" class="current">Home</a></li>
+            <li><a href="subpage.html">Search</a></li>
+            <li><a href="subpage.html">Books</a></li>            
+            <li><a href="subpage.html">New Releases</a></li>  
+            <li><a href="#">Company</a></li> 
+            <li><a href="#">Contact</a></li>
+    	</ul>
+    </div> <!-- end of menu -->
+	
+<div id="ahm_header">
+    	<div id="ahm_special_offers">
+        	<p>
+                <span>25%</span> discounts for
+        purchase over $80
+        	</p>
+			<a href="subpage.html" style="margin-left: 50px;">Read more...</a>
+        </div>
+        
+        
+        <div id="ahm_new_books">
+        	<ul>
+                <li>Suspen disse</li>
+                <li>Maece nas metus</li>
+                <li>In sed risus ac feli</li>
+            </ul>
+            <a href="subpage.html" style="margin-left: 50px;">Read more...</a>
+        </div>
+    </div> <!-- end of header -->	
+</body>
+</html>

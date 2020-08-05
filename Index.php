342c901b4962bdc7005923ae9e632049677af5d7
masororasghar1 <u1118456@umail.usq.edu.au>
2020-08-05 17:06:45 +0500

    Slider and footer added

--- Index.php
+++ Index.php
@@ -3,62 +3,47 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+
 <title>AHM Books</title>
-<style>
 
-body{
-	margin: 0;
-	padding: 0;
-	line-height: 1.5em;
-	font-family: Verdana, Arial, san-serif;
-	font-size: 11px;
-	color: #ffffff;
-	background: #DAF7A6;
-}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+<link rel="stylesheet" type="text/css" href= "ahm.css">
 	
-#ahm_container{
-	width: 960px;
-	margin: 0 auto;
-	padding: 0 10px;
-	background: #1c1c1b url(images/templatemo_bg.jpg) repeat-y;
-}	
-/* menu */
+<!--Javacript code for slider-->	
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
 
-#ahm_menu {
-	clear: both;
-	width: 960px;
-	height: 45px;
-	background: #800080;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-#ahm_menu ul {
-	padding: 13px 0 0 0;
-	margin: 0;
-	list-style: none;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-#ahm_menu ul li{
-	display: inline;
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
 
-#ahm_menu ul li a{
-	float: left;
-	padding: 0 20px;
-	font-size: 12px;
-	font-weight: bold;
-	text-align: center;
-	text-decoration: none;
-	color: #FFFFFF;
-}
+</script>
+<!--end of javascript code for slider-->
 
-#ahm_menu li a:hover, #ahm_menu li .current{
-	color: #fcf88e;
-}
-	
-	
-</style>	
-	
 </head>
+<!--end of head-->
 
 <body>
 <div id="ahm_container">
@@ -69,28 +54,51 @@ body{
             <li><a href="subpage.html">Books</a></li>            
             <li><a href="subpage.html">New Releases</a></li>  
             <li><a href="#">Company</a></li> 
-            <li><a href="#">Contact - example change </a></li>
+            <li><a href="#">Contact</a></li>
     	</ul>
     </div> <!-- end of menu -->
 	
-<div id="ahm_header">
-    	<div id="ahm_special_offers">
-        	<p>
-                <span>25%</span> discounts for
-        purchase over $80
-        	</p>
-			<a href="subpage.html" style="margin-left: 50px;">Read more...</a>
-        </div>
-        
-        
-        <div id="ahm_new_books">
-        	<ul>
-                <li>Suspen disse</li>
-                <li>Maece nas metus</li>
-                <li>In sed risus ac feli</li>
-            </ul>
-            <a href="subpage.html" style="margin-left: 50px;">Read more...</a>
-        </div>
-    </div> <!-- end of header -->	
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src= "quaid-lagan-7KWhvhFftpk-unsplash.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src= "vnwayne-fan-lzHYLfNI89A-unsplash.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src= "daria-nepriakhina-xY55bL5mZAM-unsplash.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
+<div id="ahm_footer">
+    
+	       <a href="subpage.html">Home</a> | <a href="subpage.html">Search</a> | <a href="subpage.html">Books</a> | <a href="#">New Releases</a> | <a href="#">FAQs</a> | <a href="#">Contact Us</a><br />
+        Copyright © 2020 <a href="#"><strong>AHM books</strong></a> | <a href="http://www.iwebsitetemplate.com" target="_parent">Website Templates</a> by <a href="http://www.templatemo.com" target="_parent" title="free css templates">Free CSS Templates</a>	
+</div> 
+    <!-- end of footer -->	
+
 </body>
 </html>
